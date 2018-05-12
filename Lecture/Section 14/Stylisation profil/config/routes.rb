8845f476e55f1834e3722222e7770d444f8ca2e1
gCKn <alexisolland@getcodingknowledge.com>
2018-05-12 15:31:51 +0200

    Stylisation Profil Utilisateur

---
+++ Lecture/Section 14/Stylisation profil/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root "statique#accueil"
+  get "/contact", to: "statique#contact"
+  get "/team", to: "statique#team"
+  get "/services", to: "statique#services"
+  get "/signup", to: "utilisateurs#new"
+  resources :utilisateurs
+end
